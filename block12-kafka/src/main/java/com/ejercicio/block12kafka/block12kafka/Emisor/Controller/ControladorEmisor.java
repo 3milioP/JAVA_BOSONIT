ffaa9234bf1345e220941cad6a75f362c061d470
Emilio José Pérez Arjona <emilio.parjona@bosonit.com>
2023-07-19 17:17:25 +0200

    Block15-Kafka

---
+++ block12-kafka/src/main/java/com/ejercicio/block12kafka/block12kafka/Emisor/Controller/ControladorEmisor.java
@@ -0,0 +1,2 @@
+package com.ejercicio.block12kafka.block12kafka.Emisor.Controller;public class ControladorEmisor {
+}
---
+++ block12-kafka/src/main/java/com/ejercicio/block12kafka/block12kafka/Receptor/Domain/Receptor.java
@@ -0,0 +1,2 @@
+package com.ejercicio.block12kafka.block12kafka.Receptor.Domain;public class Receptor {
+}
